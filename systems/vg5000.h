195d85d733140ecd43348aef3c11769b69acc6a8
Sylvain Glaize <mokona@puupuu.org>
2023-07-13 19:44:36 +0200

    vg5000: fix license for this emulator code

--- systems/vg5000.h
+++ systems/vg5000.h
@@ -4,24 +4,23 @@
 
     A VG5000 emulator in a C header.
 
-    Do this:
-    ~~~C
-    #define CHIPS_IMPL
-    ~~~
-    before you include this file in *one* C or C++ file to create the
-    implementation.
+    ## zlib/libpng license
 
-    Optionally provide the following macros with your own implementation
-
-    ~~~C
-    CHIPS_ASSERT(c)
-    ~~~
-        your own assert macro (default: assert(c))
-
-    You need to include the following headers before including vg5000.h:
-
-        - TODO: provide list of dependencies
-
+    Copyright (c) 2023 Sylvain Glaize
+    This software is provided 'as-is', without any express or implied warranty.
+    In no event will the authors be held liable for any damages arising from the
+    use of this software.
+    Permission is granted to anyone to use this software for any purpose,
+    including commercial applications, and to alter it and redistribute it
+    freely, subject to the following restrictions:
+        1. The origin of this software must not be misrepresented; you must not
+        claim that you wrote the original software. If you use this software in a
+        product, an acknowledgment in the product documentation would be
+        appreciated but is not required.
+        2. Altered source versions must be plainly marked as such, and must not
+        be misrepresented as being the original software.
+        3. This notice may not be removed or altered from any source
+        distribution. 
 #*/
 
 #include <stdint.h>
