ab040819bdc01448e9b52d865a40b48daba67c18
Sylvain Glaize <mokona@puupuu.org>
2023-07-22 02:03:53 +0200

    vg5000: compute quadrants for double width, height and size (ef9345)

--- chips/ef9345.h
+++ chips/ef9345.h
@@ -222,7 +222,8 @@ typedef struct {
     ef9345_char_code_t char_code;
     uint8_t block_origin;
     uint8_t origin_row_yor;
-    ef945_char_triplet_t row_buffer[40]; // TODO to verify when implementing the 80 char mode
+    ef945_char_triplet_t row_buffer[40];
+    uint8_t quadrant_buffer[40]; // used for Double Height and Double Width attribute
 
     uint8_t latest_loaded_row_line;
     uint8_t latest_rendered_column;
@@ -700,7 +701,7 @@ static void _ef9345_load_char_row_40_short(ef9345_t* ef9345, uint8_t row) {
                 const uint8_t color    = (data_a_prime & 0b00000111) << 4;
                 const uint8_t flash    = (data_a_prime & 0b00001000);
                 const uint8_t height   = (data_a_prime & 0b00010000) >> 3;
-                const uint8_t width    = (data_a_prime & 0b00100000) >> 1;
+                const uint8_t width    = (data_a_prime & 0b00100000) >> 2;
                 const uint8_t negative = (data_a_prime & 0b01000000) << 1;
                 const uint8_t in_ram   = (data_b_prime & 0b10000000);
                 data_a = negative | color | flash | latched_background_color;
@@ -790,6 +791,9 @@ static uint64_t _ef9345_beam_update(ef9345_t* ef9345, uint64_t vdp_pins) {
 
     const uint16_t active_scan_lines = 250;
     const uint16_t first_active_line = last_scan_line - active_scan_lines;
+    const uint16_t next_line = ef9345->current_line;
+    const int16_t  current_row = (next_line - first_active_line) / 10;
+
 
     // Loads the next character row in intermediary buffer during the start of a row
     // TODO: be closer to actual loading timings.
@@ -799,13 +803,66 @@ static uint64_t _ef9345_beam_update(ef9345_t* ef9345, uint64_t vdp_pins) {
     // two alternating buffers.
     if (ef9345->latest_loaded_row_line != ef9345->current_line) {
         ef9345->latest_loaded_row_line = ef9345->current_line;
-
-        const uint16_t next_line = ef9345->current_line;
         
         if ((next_line >= first_active_line && (next_line % 10) == 0)) {
-            uint8_t row = (next_line - first_active_line) / 10;
-            if (row < 24) {
-                _ef9345_load_char_row(ef9345, row);
+
+            if (current_row == 0) {
+                memset(ef9345->quadrant_buffer, 0, sizeof(ef9345->quadrant_buffer));
+            }
+
+            if (current_row < 24) {
+                _ef9345_load_char_row(ef9345, current_row);
+
+                // Compute quadrant for double width/height
+                for (uint8_t x = 0; x < 40; x++) {
+                    const bool is_double_height = ef9345->row_buffer[x].b & 0x02;
+                    const bool is_double_width = ef9345->row_buffer[x].b & 0x08;
+                    const bool is_double_size = is_double_height && is_double_width;
+
+                    // Check quadrant
+                    // 0 : normal size
+                    // 5 6
+                    // 9 A: double size
+                    // 1 2 : double width
+                    // 4
+                    // 8 : double height
+                    uint8_t quadrant = 0;
+
+                    if (x > 0)
+                    {
+                        if (ef9345->quadrant_buffer[x-1] == 5 && is_double_size) {
+                            quadrant = 6;
+                        }
+                        else if (ef9345->quadrant_buffer[x-1] == 9 && is_double_size) {
+                            quadrant = 0xA;
+                        }
+                        else if (ef9345->quadrant_buffer[x-1] == 1 && is_double_width) {
+                            quadrant = 2;
+                        }
+                        else if (ef9345->quadrant_buffer[x] == 4 && is_double_height) {
+                            quadrant = 8;
+                        }
+                    }
+
+                    if (quadrant == 0) {
+                        if (is_double_size) {
+                            if (ef9345->quadrant_buffer[x] == 5) {
+                                quadrant = 9;
+                            }
+                            else {
+                                quadrant = 5;
+                            }
+                        }
+                        else if (is_double_width) {
+                            quadrant = 1;
+                        }
+                        else if (is_double_height) {
+                            quadrant = 4;
+                        }
+                    }
+
+                    ef9345->quadrant_buffer[x] = quadrant;
+                }
             }
         }
     }
@@ -820,9 +877,8 @@ static uint64_t _ef9345_beam_update(ef9345_t* ef9345, uint64_t vdp_pins) {
             if (x != ef9345->latest_rendered_column) {
                 ef9345->latest_rendered_column = x;
 
-                // At now, only bicolor 40 char/row is supported
+                // At now, only bicolor alpha 40 char/row is supported
                 const uint8_t char_from_buffer = (ef9345->row_buffer[x].c) & 0x7f;
-                //const uint8_t char_from_buffer = x + 64;
                 const uint8_t colors_from_buffer = ef9345->row_buffer[x].a;
 
                 uint32_t fb_address = ((ef9345->current_line - first_active_line) * EF9345_FRAMEBUFFER_WIDTH) + (x * 8);
@@ -837,19 +893,35 @@ static uint64_t _ef9345_beam_update(ef9345_t* ef9345, uint64_t vdp_pins) {
                     }
                 }
                 else {
+                    // Render character
+                    const uint8_t  quadrant = ef9345->quadrant_buffer[x];
                     const uint16_t char_offset = ((char_from_buffer & 0x7f) >> 2) * 0x40 +
                                                 (char_from_buffer & 0x03);
                     const uint16_t base_char_address = 0x0800;
+                    const uint16_t slice_height_shift = quadrant & 0b1000 ? 5 : 0;
+                    const uint16_t slice_number = ((ef9345->current_line % 10) / ((quadrant & 0b1100) ? 2 : 1)) +
+                                                  slice_height_shift;
                     const uint16_t slice_address = base_char_address +
                                                 char_offset +
-                                                ((ef9345->current_line % 10) * 4);
+                                                (slice_number * 4);
 
                     uint16_t slice_value = mem_rd(&ef9345->charset_mem, slice_address);
 
-                    for (size_t pixel = 0; pixel < 8; pixel++) {
-                        uint8_t value = (slice_value & 0x01) ? fg_color : bg_color;
-                        ef9345->fb[fb_address++] = value;
-                        slice_value >>= 1;
+                    if (quadrant & 0x03) {
+                        if (quadrant & 0x02) { slice_value >>= 4; }
+                        for (size_t pixel = 0; pixel < 8; pixel+=2) {
+                            uint8_t value = (slice_value & 0x01) ? fg_color : bg_color;
+                            ef9345->fb[fb_address++] = value;
+                            ef9345->fb[fb_address++] = value;
+                            slice_value >>= 1;
+                        }
+                    }
+                    else {
+                        for (size_t pixel = 0; pixel < 8; pixel++) {
+                            uint8_t value = (slice_value & 0x01) ? fg_color : bg_color;
+                            ef9345->fb[fb_address++] = value;
+                            slice_value >>= 1;
+                        }
                     }
                 }
             }
